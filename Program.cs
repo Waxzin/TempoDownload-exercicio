82e6f2a12954dc2d9a0a95f2e74460a89fc32d26
matheus <matheusluan04@gmail.com>
2026-04-22 21:44:19 -0300

    Finalização de atividade

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿Console.WriteLine("TEMPO DE DOWNLOAD");
 
-Console.WriteLine("me informe o tamanho do arquivo em MB");
-string tamanhoArquivo = Console.ReadLine()!;
+double tamanhoArquivo, velocidadeMbps, tempo;
 
-string velocidadeArquivo = 
+Console.WriteLine("Tamanho do arquivo em MB...:");
+tamanhoArquivo = Convert.ToDouble(Console.ReadLine());
+
+ Console.WriteLine("Velocidade da conexão em Mbps...:");
+ velocidadeMbps = Convert.ToDouble(Console.ReadLine());
+
+ tempo = tamanhoArquivo * 8 / velocidadeMbps / 60;
+
+Console.WriteLine($"\nTempo estimado de download: {tempo:N1}minutos");
+
